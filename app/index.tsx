53d080c694167de58b962d93bb773baa53edb864
shahnoorgit <shahnoormujawar@gmail.com>
2024-08-01 22:56:32 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const index = () => {
+  return (
+    <View className=" flex-1 justify-center items-center bg-gray-700">
+      <Text className=" text-white font-bold">Hello world</Text>
+    </View>
+  );
+};
+
+export default index;
