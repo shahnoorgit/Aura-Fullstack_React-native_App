b252000f3024c47107ed519198647029d9bcf1a9
shahnoorgit <shahnoormujawar@gmail.com>
2024-08-04 21:19:33 +0530

    onBoarding screen done

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,56 @@
-import { Link } from "@react-navigation/native";
+import { Redirect, router } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
-
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomBtn from "../components/Custombtn";
 const index = () => {
   return (
-    <View className=" flex-1 justify-center items-center ">
-      <Text className="text-3xl font-pblack">Aura!</Text>
-      <Link to={"/home"}>
-        <Text className=" text-blue-500">Go To Home</Text>
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[298px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"}
+              Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with Aora
+          </Text>
+          <CustomBtn
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles={"w-full mt-7"}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
